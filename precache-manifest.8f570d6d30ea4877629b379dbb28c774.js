f01e04da66da400eddbd11cc553c49107639bb3f
Vladislav Chudak <chudiklolz@gmail.com>
2019-05-30 01:04:38 +0100

    Updates

---
+++ precache-manifest.8f570d6d30ea4877629b379dbb28c774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edebf7ac93afc41de2721103a371b53",
+    "url": "/static/media/logo.8edebf7a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4473f7629145f6885e56",
+    "url": "/static/js/main.4473f762.chunk.js"
+  },
+  {
+    "revision": "1a64346927b2f14d6588",
+    "url": "/static/js/1.1a643469.chunk.js"
+  },
+  {
+    "revision": "4473f7629145f6885e56",
+    "url": "/static/css/main.33860e0f.chunk.css"
+  },
+  {
+    "revision": "9e49f44c90d340f575a68671b668fe04",
+    "url": "/index.html"
+  }
+];
